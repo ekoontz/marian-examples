b519b765da11dd29f002d033f9e19f4cf740831e
Eugene Koontz <ekoontz@hiro-tan.org>
2023-05-21 21:22:59 +0000

    use same whitespace conventions as runme.sh

--- transformer-intro/scripts/train.sh
+++ transformer-intro/scripts/train.sh
@@ -5,8 +5,12 @@ export TRG=en SRC=de GPU="0"
 
 $MARIAN/marian \
      -c transformer-model.yml \
-     -d ${GPU} --workspace 9000 --seed 1111 --after 10e \
-     --model model/model.npz    --train-sets data/corpus.clean.{$SRC,$TRG} \
-     --vocabs model/vocab.$SRC$TRG.spm model/vocab.$SRC$TRG.spm --dim-vocabs 32000 32000 \
+     -d ${GPU} --workspace 9000 \
+     --seed 1111 \
+     --after 10e \
+     --model model/model.npz \
+     --train-sets data/corpus.clean.{$SRC,$TRG} \
+     --vocabs model/vocab.$SRC$TRG.spm model/vocab.$SRC$TRG.spm \
+     --dim-vocabs 32000 32000 \
      --valid-sets data/valid.{$SRC,$TRG}
      --log model/train.log --valid-log model/valid.log
